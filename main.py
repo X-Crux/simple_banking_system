1b6277eb0a111caf760012f54e61d56c8b263d6a
Dmitriy Shcheglov <dmitriy.kras@gmail.com>
2021-02-25 16:02:39 +0700

    Добавлена БД SQLite

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sys
 from random import randint
+import sqlite3
 
 
 def creat_card():
     card_number = algorithm_luhn()
     card_pin = str(randint(10000, 19999))[1:]
-    if card_number not in db.keys():
-        db[card_number] = [card_pin, 0]
+    all_numbers = [''.join(i) for i in cur.execute(
+        'SELECT number FROM card'
+    )]
+    if card_number not in all_numbers:
+        cur.execute(
+            f'INSERT INTO card (number, pin)'
+            f'VALUES ({card_number}, {card_pin})'
+        )
+        conn.commit()
         print('\nYour card has been created')
         print(f'Your card number:\n{card_number}\n'
               f'Your card PIN:\n{card_pin}\n')
@@ -40,8 +48,14 @@ def log_in():
     card_number = str(input('\nEnter your card number:\n'))
     card_pin = str(input('Enter your PIN:\n'))
     try:
-        if db[card_number][0] == card_pin:
-            log_info(db[card_number][1])
+        get_pin = [i for i in cur.execute(
+            f'SELECT pin FROM card WHERE number={card_number}'
+        )][0][0]
+        if get_pin == card_pin:
+            get_balance = [i for i in cur.execute(
+                f'SELECT balance FROM card WHERE number={card_number}'
+            )][0][0]
+            log_info(get_balance)
         else:
             print('\nWrong card number or PIN!\n')
     except Exception:
@@ -61,6 +75,7 @@ def log_info(balance):
             break
         elif choice == '0':
             print('\nBye!')
+            conn.close()
             sys.exit(0)
         else:
             print('\nWrong menu number!\n')
@@ -76,11 +91,23 @@ def start_menu():
             log_in()
         elif choice == '0':
             print('\nBye!')
+            conn.close()
             sys.exit(0)
         else:
             print('\nWrong menu number!\n')
 
 
 if __name__ == '__main__':
-    db = {}
+    conn = sqlite3.connect('card.s3db')
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS card ("
+        "id INTEGER PRIMARY KEY,"
+        "number TEXT,"
+        "pin TEXT,"
+        "balance INTEGER DEFAULT 0"
+        ")"
+    )
+    conn.commit()
     start_menu()
+    conn.close()
